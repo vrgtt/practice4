b7dd03828c90e9b13af38b06b2e6393985ddde63
vrgtt <ananaxya@mail.ru>
2024-01-03 15:27:02 +0300

    Added tests

--- calculate_salary.test.js
+++ calculate_salary.test.js
@@ -9,3 +9,8 @@ test('worker salary', function () {
     let salary = calculate_salary('worker');
     expect(salary).toBe(1500);
 });
+
+test('accountant salary', function () {
+    let salary = calculate_salary('accountant');
+    expect(salary).toBe(2500);
+});
